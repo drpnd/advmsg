1bfb0ee5e567ff652b2e2975b4d6281d09d205f2
Hirochika Asai <panda@jar.jp>
2021-12-05 23:24:38 +0900

    Add idn/agent.py

---
+++ idn/agent.py
@@ -0,0 +1,18 @@
+import argparse
+
+# Arguments
+parser = argparse.ArgumentParser()
+
+"""
+Main routine
+"""
+def main(args):
+    return True
+
+"""
+Call the main routine
+"""
+if __name__ == "__main__":
+    # Parse the arguments
+    args = parser.parse_args()
+    main(args)
