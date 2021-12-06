ad05fbb211bea40b0e3f2fb5cb6cc914ca3bf0d8
Hirochika Asai <panda@jar.jp>
2021-12-06 20:50:12 +0900

    Implementing the ID network agent

--- idn/agent.py
+++ idn/agent.py
@@ -1,13 +1,26 @@
 import argparse
+import socket
 
 # Arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--port', type=int, default=10914)
+parser.add_argument('--bind-address', type=str, default="0.0.0.0")
+parser.add_argument('--bind-port', type=int, default=10914)
 
 """
 Main routine
 """
 def main(args):
+
+    # Open a socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((args.bind_address, args.bind_port))
+    sock.listen(10)
+    while True:
+        client, caddr = sock.accept()
+        print(client, caddr)
+        pass
+    sock.close()
+
     return True
 
 """
