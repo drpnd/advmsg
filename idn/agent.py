298f4d0be2c36ef517c537839a5f015ae6dc55db
Hirochika Asai <panda@jar.jp>
2021-12-07 22:41:40 +0900

    Implementing TCP server for the ID agent.

--- idn/agent.py
+++ idn/agent.py
@@ -1,29 +1,61 @@
 import argparse
 import socket
+import threading
 
 # Arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--bind-address', type=str, default="0.0.0.0")
 parser.add_argument('--bind-port', type=int, default=10914)
 
+NUM_WORKERS = 10
+
+"""
+Thread
+"""
+class ClientThread(threading.Thread):
+    def __init__(self, csock):
+        threading.Thread.__init__(self)
+        self.csock = csock
+        self.running = True
+
+    def run(self):
+        while self.running:
+            data = self.csock.recv(4096)
+            print(data)
+            self.csock.send(data)
+            if len(data) == 0:
+                self.running = False
+
+    def stop(self):
+        print("Closing the socket")
+        self.running = False
+        self.csock.close()
+
 """
 Main routine
 """
 def main(args):
-
     # Open a socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((args.bind_address, args.bind_port))
-    sock.listen(10)
+    sock.listen(NUM_WORKERS)
+    threads = []
+    main_thread = threading.current_thread()
     while True:
-        client, caddr = sock.accept()
-        print(client, caddr)
-        data = client.recv(4096)
-        print(data)
-        client.send(data)
-        client.close()
-        pass
+        csock, caddr = sock.accept()
+        print(csock, caddr)
+        th = ClientThread(csock)
+        th.start()
+        threads.append(th)
+
+        for th in threading.enumerate():
+            if th == threading.main_thread():
+                continue
+            if not th.is_alive():
+                th.join()
+            print(th, th.is_alive())
+
     sock.close()
 
     return True
