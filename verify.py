5fab2f2b9041bedec24a381a285a9264da6ede3a
Hirochika Asai <panda@jar.jp>
2021-12-09 20:02:32 +0900

    Implement verification.

--- verify.py
+++ verify.py
@@ -9,6 +9,7 @@ from cryptography.hazmat.primitives import serialization
 # Arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--cacert', type=str, default="ca/cacert.pem")
+parser.add_argument('--crt', type=str, default="crt.pem")
 
 """
 Main routine
@@ -18,6 +19,22 @@ def main(args):
     with open(args.cacert, 'rb') as f:
         cacert_pem = f.read()
         f.close()
+    cacert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cacert_pem)
+
+    # Load th certificate
+    with open(args.crt, 'rb') as f:
+        cert_pem = f.read()
+        f.close()
+    cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert_pem)
+
+    try:
+        store = OpenSSL.crypto.X509Store()
+        store.add_cert(cacert)
+        store_ctx = OpenSSL.crypto.X509StoreContext(store, cert)
+        store_ctx.verify_certificate()
+    except Exception as e:
+        return False
+
     return True
 
 """
